bd55ccf6d3db0fb907bfad4466dd83acf03066bb
DevGGamer <76408335+DevGGamer@users.noreply.github.com>
2024-12-08 21:25:28 +0300

    dadwa

--- Frontend/src/Components/Form/Form.jsx
+++ Frontend/src/Components/Form/Form.jsx
@@ -14,7 +14,7 @@ function Form()
         event.preventDefault();
 
         try {
-            const response = await fetch('https://5.35.11.70:3000/send-email', { 
+            const response = await fetch('https://5.35.11.70/send-email', { 
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
