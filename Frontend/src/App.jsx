6f179f7fe8dda75d995fa689b401725b5afe0bf2
DevGGamer <76408335+DevGGamer@users.noreply.github.com>
2024-12-08 16:04:53 +0300

    Remove Attention Block

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import Attention from './Components/Attention/Attention';
 import Header from './Components/Header/Header';
 import AboutPage from './Pages/About/AboutPage'
 import ServicesPage from './Pages/Services/ServicesPage'
@@ -11,7 +10,6 @@ import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <>
-      <Attention attentionText={"Сайт находится на этапе разработки!"}/>
       <div id='app'>
         <BrowserRouter>
             <Header />
