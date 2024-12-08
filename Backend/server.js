1281c3d3edf4cf8317bf1de46ee772b6fd06926c
DevGGamer <76408335+DevGGamer@users.noreply.github.com>
2024-12-09 00:50:43 +0300

    add docker

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 8080;
 
 app.use(cors({
-    origin: ['https://ereminawebsite.pro', 'http://localhost:5173']
+    origin: 'https://ereminawebsite.pro'
 }));
 app.use(express.json());
 
