f7726d78ca2c8fe58f3ea4329855b28ed26f3c72
DevGGamer <76408335+DevGGamer@users.noreply.github.com>
2024-12-08 21:22:18 +0300

    back

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = 3000;
 const corsOptions = {
-    origin : ['https://5.35.11.70:3000', 'https://ereminawebsite.pro:3000'],
+    origin : ['https://5.35.11.70', 'https://ereminawebsite.pro'],
  }
 
 app.use(cors(corsOptions));
