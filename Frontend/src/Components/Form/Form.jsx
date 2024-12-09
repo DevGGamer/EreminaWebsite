58ed646ca9ae878401ec4aa96e5d6d229f0613db
DevGGamer <76408335+DevGGamer@users.noreply.github.com>
2024-12-09 10:05:55 +0300

    form

--- Frontend/src/Components/Form/Form.jsx
+++ Frontend/src/Components/Form/Form.jsx
@@ -14,7 +14,7 @@ function Form()
         event.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:8080/api/send-email', { 
+            const response = await fetch('https://ereminawebsite.pro/api/send-email', { 
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
